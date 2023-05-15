ea6478eee497a25b61b6709dde8013e76a419e3a
20dce095 <20dce095@charusat.edu.in>
2023-05-15 15:21:34 +0530

    first commit for checkkkkkkk

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <Navbar fixed="bottom" bg="dark" variant="dark">  
         <Container  className="text-center text-muted">
             <Col lg={12} className="text-center text-light">
-            {year} - All Rights reserved by Pradip Pipaliya DevOps....
+            {year} - All Rights reserved by Pradip Pipaliya DevOps
             </Col>
         </Container>
       </Navbar>
